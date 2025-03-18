1a5bd9794bccc7a942e3f3744241bbab75907a9f
Kim Fransson <kim.fransson.22@gmail.com>
2025-03-18 14:34:27 +0100

    project init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import GameHeader from "./components/GameHeader";
+
+function App() {
+  return (
+    <>
+      <GameHeader />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/CurrentScore.tsx
@@ -0,0 +1,10 @@
+const CurrentScore = () => {
+  return (
+    <span className="currentScore">
+      score
+      <span aria-label="0">0</span>
+    </span>
+  );
+};
+
+export default CurrentScore;
