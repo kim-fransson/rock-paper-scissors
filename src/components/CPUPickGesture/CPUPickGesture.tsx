49a768046b1f2091a26eb2254bcc7aec52727111
Kim Fransson <kim.fransson.22@gmail.com>
2025-04-01 12:47:14 +0200

    Some more hacky / fast animation decisions

--- src/components/CPUPickGesture/CPUPickGesture.tsx
+++ src/components/CPUPickGesture/CPUPickGesture.tsx
@@ -4,14 +4,19 @@ import { useApp } from "../../hooks";
 import { Gesture } from "../Gesture";
 
 import styles from "./CPUPickGesture.module.scss";
+import { motion } from "motion/react";
+import { useMedia } from "react-use";
 
 export const CPUPickGesture = () => {
   const { state, send } = useApp();
+  const isWide = useMedia("(min-width: 768px)");
 
   const isPlayerWinner =
     state.hasTag("gameOver") && state.context.winner === state.context.player;
   const isCpuWinner =
     state.hasTag("gameOver") && state.context.winner === state.context.cpu;
+
+  const PushThem = state.hasTag("gameOver") && isWide;
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
@@ -23,15 +28,27 @@ export const CPUPickGesture = () => {
 
   return (
     <div className={styles.playerCpuGestureWrapper}>
-      <div className={`${styles.textGestureWrapper} ${styles.player}`}>
+      <motion.div
+        animate={{
+          x: PushThem ? -50 : 0,
+        }}
+        transition={{ type: "spring", stiffness: 100 }}
+        className={`${styles.textGestureWrapper} ${styles.player}`}
+      >
         <Gesture gesture={state.context.player!} isWinner={isPlayerWinner} />
         <span>you picked</span>
-      </div>
+      </motion.div>
 
-      <div className={`${styles.textGestureWrapper} ${styles.cpu}`}>
+      <motion.div
+        animate={{
+          x: PushThem ? 50 : 0,
+        }}
+        transition={{ type: "spring", stiffness: 100 }}
+        className={`${styles.textGestureWrapper} ${styles.cpu}`}
+      >
         <Gesture gesture={state.context.cpu!} isWinner={isCpuWinner} />
         <span>the house picked</span>
-      </div>
+      </motion.div>
     </div>
   );
 };
